fd65cd99b552d5c366a15b6c596d5beec02943da
sandeepbilthare <sandeep.bilthare@gmail.com>
2020-06-20 09:10:19 +0530

    Removed soem code

--- test.py
+++ test.py
@@ -1,14 +1,5 @@
-
-import math
-import sys
-
 import requests
 
-# print(sys.version)
-# print(sys.executable)
-
-# r = requests.get("https://www.google.com")
-# print(r.status_code)
-
-name = input("Your name: ?")
-print("Hello", name)
+r = requests.get("https://www.google.com")
+print(r.status_code)
+print(r.ok)
