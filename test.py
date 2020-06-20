fb871dab2dbc811114d34dd4aef2f86565f4925f
sandeepbilthare <sandeep.bilthare@gmail.com>
2020-06-20 09:03:34 +0530

    Initial commit

---
+++ test.py
@@ -0,0 +1,14 @@
+
+import math
+import sys
+
+import requests
+
+# print(sys.version)
+# print(sys.executable)
+
+# r = requests.get("https://www.google.com")
+# print(r.status_code)
+
+name = input("Your name: ?")
+print("Hello", name)
